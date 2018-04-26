36b5ace132851d9cd60445cb09df7983d24d4e81
sophiewxh <huiwang8921@gmail.com>
2018-04-26 18:14:34 -0400

    process all tasks

--- HR_Tools/hr_tmp_load_clean.m
+++ HR_Tools/hr_tmp_load_clean.m
@@ -1,31 +1,54 @@
 clear all
-dir_current = pwd();
-addpath(fullfile(dir_current, 'HeartRate', 'HR_Tools'));
-addpath(fullfile(dir_current, 'HeartRate', 'HR_Data'));
+dir = pwd();
+addpath(fullfile(dir, 'HeartRate', 'HR_Tools'));
+addpath(fullfile(dir, 'HeartRate', 'HR_Data'));
 
 
-% read xlsx file
-d = xlsread('lwp_0019_firstbeat_rr_analysis_raw_data.xlsx', 'Task 1 Relax Music');
-RR = d(:,1);
-RR_t = d(:,2);
+fp_in = 'lwp_0019_firstbeat_rr_analysis_raw_data.xlsx';
+task = 'Task 1 Relax Music';
+clean(dir, fp_in, task);
 
-% poincare plot before cleaning
-plotflag = 1;
-[sd1, sd2] = hr_poincare(RR, plotflag);
-title('Subj 19 - Raw data', 'Interpreter', 'none');
 
-% clean and smoothing
-win = 25; 
-plotflag = 0;
-[RR_t_clean, RR_clean] = hr_clean(RR_t, RR, win, plotflag);
+% [~, sheet_names] = xlsfinfo(fp_in);
+% num_task = length(sheet_names);
+% for i = 1:num_task
+%     task_name = sheet_names(i);
+%     clean(fp_in, task_name);
+    
 
-% poincare plot after cleaning
-plotflag = 1;
-[sd1, sd2] = hr_poincare(RR_clean, plotflag);
-title('Subj 19 - Clean Data', 'Interpreter', 'none');
+   
+function clean(dir, file_name, task_name)
+    % read rr and rr_t of a task 
+    d = xlsread(file_name, task_name);
+    RR = d(:,1);
+    RR_t = d(:,2);
+    
+    % poincare plot before cleaning
+    plotflag = 1;
+    [sd1, sd2] = hr_poincare(RR, plotflag);
+    title('Subj 19 - Raw data', 'Interpreter', 'none');
+    
+    % clean and smoothing
+    win = 25; 
+    plotflag = 0;
+    [RR_t_clean, RR_clean] = hr_clean(RR_t, RR, win, plotflag);
 
-% write to ibi file
-fp_out = fullfile(dir_current, 'HeartRate', 'HR_Data', 'sophie_test.ibi');
-ibi_file = fopen(fp_out, 'w');
-fprintf(ibi_file,'%.3f\n', RR_clean./1000);
-fclose(ibi_file);
+    % poincare plot after cleaning
+    plotflag = 1;
+    [sd1, sd2] = hr_poincare(RR_clean, plotflag);
+    title('Subj 19 - Clean Data', 'Interpreter', 'none');
+
+    % write to ibi file
+    
+    fp_out = fullfile(dir, 'HeartRate', 'HR_Data', sprintf('lwp0019_FirstBeat_RR_%s.ibi', strrep(task_name, ' ', '')));
+    ibi_file = fopen(fp_out, 'w');
+    fprintf(ibi_file,'%.3f\n', RR_clean./1000);
+    fclose(ibi_file);
+end
+
+
+    
+
+
+
+
